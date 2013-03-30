08067814bf3c0a1cbe648e3dd98cf2f33edba917
Pranav Ravichandran <me@onloop.net>
2013-03-30 21:53:43 +0530

    Added to json variable if count exceeds.

--- proofofconcept/index.js
+++ proofofconcept/index.js
@@ -1,14 +1,29 @@
 var global = (function () {
 	var content = "";
+	var possibleSearchUrls = {};
+	var searchParam;
+	var responseDiv;
 	
 	return {
 		submitForm: function () {
 			content = document.getElementById("urlText").value;
+			responseDiv = document.getElementById('response');
 
-			if (this.isValidUrl(content) && this.isSearchUrl(content)) {
-				alert("Valid Search Url.");
+			if (this.isValidUrl(content) && this.isSearchUrl()) {
+				var searchUrl = content.substr(0, content.indexOf("?")) + "?" + searchParam + "=";
+
+				if (possibleSearchUrls.searchUrl) {
+					possibleSearchUrls.searchUrl += 1;
+
+					if (possibleSearchUrls.searchUrl == 3) {
+						if (confirm("New Search Engine found at '" + searchUrl + "' Add?"))
+							responseDiv.innerHTML += '<input type="radio" name="engine" value="' + searchUrl + '">' + searchUrl + '<br />';
+					}
+				} else {
+					possibleSearchUrls.searchUrl = 1;
+				}
+
 			} else {
-				alert("Not a valid Search Url.");
 			}
 		},
 
@@ -20,10 +35,13 @@ var global = (function () {
 							var param = decodeURI(
 								(RegExp(name + '=' + '(.+?)(&|$)').exec(content)||[,null])[1]
 								);
-					
-							return (param == "null") ? false: param;
+							
+							if (param != "null") searchParam = name;	
+							return (param == "null") ? false : param;
 						}
-						)) {
+						)
+			   )
+			{
 				return true;
 			} else {
 				return false;
